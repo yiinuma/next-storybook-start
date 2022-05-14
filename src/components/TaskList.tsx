7951e2e909b3455ef4a88456833520bc73fd8ce9
yiinuma <y.iinuma211@gmail.com>
2022-05-14 14:33:28 +0900

    change title-message

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -42,7 +42,7 @@ export const TaskList = ({ loading, tasks, onPinTask, onArchiveTask }: Props) =>
       <div className="list-items">
         <div className="wrapper-message">
           <span className="icon-check" />
-          <div className="title-message">You have no tasks</div>
+          <div className="title-message">You have no tasks!</div>
           <div className="subtitle-message">Sit back and relax</div>
         </div>
       </div>
