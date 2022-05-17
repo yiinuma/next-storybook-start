48817de4b4a431f28f01e82097ff6880d0710a7a
yiinuma <y.iinuma211@gmail.com>
2022-05-17 19:20:22 +0900

    snapshot  update

--- src/components/Task.stories.tsx
+++ src/components/Task.stories.tsx
@@ -20,7 +20,7 @@ const defaultTask: TaskType = {
 export const Default: Story = {
   args: { task: defaultTask },
 };
-const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+const longTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!!`;
 
 export const Pinned: Story = {
   args: { task: { ...defaultTask, state: 'TASK_PINNED' } },
